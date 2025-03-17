66ab1c958d852ddd4876f86f0d187a71fb38984e
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-16 20:21:49 -0400

    Add address & temp phone number to footer

---
+++ src/app/Footer.tsx
@@ -0,0 +1,17 @@
+import "react";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <ul className="footerContent">
+        <li>주소: 212 School St, Somerville, MA 02145</li>
+        <li>전화: 123-456-7890</li>
+        <li>ⓒMarpe Church. All Rights Reserved.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
