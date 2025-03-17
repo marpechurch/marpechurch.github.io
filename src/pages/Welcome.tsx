fd34e963c2aeaf852734a1e61c634529207c5ab5
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-16 23:41:47 -0400

    Set sticky header, add content to Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -4,13 +4,28 @@ import "./Welcome.css";
 
 export default function Welcome() {
   return (
-    <div className="welcome">
+    <>
       <img
         src="https://images.unsplash.com/photo-1600577916048-804c9191e36c?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        className="welcomeImg"
         alt="Welcome"
       />
-      <h1 className="welcomeText">Welcome</h1>
-    </div>
+      <h1>교회 소개</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie
+        suscipit nunc, ut iaculis nunc lobortis vitae. Sed malesuada eleifend
+        erat, vel congue dolor sollicitudin vel. Aliquam cursus mattis massa.
+        Nam elementum ex aliquet eros auctor, eu placerat nisi sagittis. Etiam
+        varius, nulla ac aliquet porttitor, augue mi eleifend felis, ut tempus
+        ipsum lectus in metus. Nam elementum semper sagittis. Morbi lacinia
+        condimentum diam sed blandit. Integer elementum lectus eu vestibulum
+        sollicitudin. Suspendisse potenti. Praesent ac nisi sit amet libero
+        interdum facilisis lacinia non neque. Mauris egestas venenatis eros, a
+        iaculis arcu mollis in. Donec odio purus, tristique at feugiat quis,
+        vulputate non orci. Praesent quis ante eu lectus lacinia volutpat
+        placerat at diam. Phasellus molestie pharetra ultrices. Phasellus
+        finibus tortor lacinia neque efficitur molestie. Duis lobortis sit amet
+        tellus vitae pellentesque.
+      </p>
+    </>
   );
 }
