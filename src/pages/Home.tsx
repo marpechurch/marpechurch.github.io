f4e2298f2427ce834c86c198210560c57dae06f9
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-24 21:23:41 -0400

    Update welcome page image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import "react";
 
-import church from "../assets/church.jpg";
+import marpeLogo from "../assets/marpe-logo.png";
 
 import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home">
-      <img src={church} alt="Church" /> 
+      <img src={marpeLogo} alt="Church" /> 
       <h1 className="welcome">마르페 교회로 환영합니다!</h1>
     </div>
   );
