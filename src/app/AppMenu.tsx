57f8dfab44fa5a8016c9b8ed3be0ade681989aad
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-24 22:17:38 -0400

    Using MUI (WIP)

--- src/app/AppMenu.tsx
+++ src/app/AppMenu.tsx
@@ -1,23 +1,123 @@
-import "react";
+import { useState } from "react";
 import {
+  Mail as MailIcon,
+  MoveToInbox as InboxIcon,
+} from "@mui/icons-material";
+import {
+  Box,
   Button,
-  Menu,
-  MenuItem,
-  MenuTrigger,
-  Popover,
-} from "react-aria-components";
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
 import { Link } from "react-router";
 
 import "./AppMenu.css";
 
-function AppMenu() {
+export default function AppMenu() {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <div className="appMenu">
-      <MenuTrigger>
-        <Button aria-label="Menu" className="menuButton">
+      <Button onClick={toggleDrawer(true)}>☰</Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      {
+        <ul className="menu">
+          <li>
+            <Link className="link" to="church">
+              교회 소개
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="community">
+              공동체 소개
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="service">
+              예배 안내
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="videos">
+              설교 영상
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="offering">
+              헌금 안내
+            </Link>
+          </li>
+        </ul>
+      }
+      {/* <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Info</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav> */}
+      {/* <MenuTrigger> */}
+      {/* <Button aria-label="Menu" className="menuButton">
           ☰
-        </Button>
-        <Popover>
+        </Button> */}
+      {/* <Popover>
           <Menu className="menu">
             <MenuItem>
               <Link className="link" to="church">
@@ -45,10 +145,8 @@ function AppMenu() {
               </Link>
             </MenuItem>
           </Menu>
-        </Popover>
-      </MenuTrigger>
+        </Popover> */}
+      {/* </MenuTrigger> */}
     </div>
   );
 }
-
-export default AppMenu;
