168d52d6f1a1b47b7002f34ab9df807287a82690
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-27 22:53:32 -0400

    Update Church text

--- src/pages/Church.tsx
+++ src/pages/Church.tsx
@@ -5,23 +5,31 @@ import "./Church.css";
 export default function Church() {
   return (
     <>
-      <img src="https://files.oaiusercontent.com/file-JJBuARL69vyb19Um8QSvZk?se=2025-03-25T01%3A21%3A00Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D1be1e093-2dbb-4bca-999d-00338697a84b.webp&sig=0VqxLM1oqU1A9YjQhv5u5THU4qI7pABcg6ElpT5E%2BnM%3D" alt="Church" />
+      <img
+        src="https://files.oaiusercontent.com/file-JJBuARL69vyb19Um8QSvZk?se=2025-03-25T01%3A21%3A00Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D1be1e093-2dbb-4bca-999d-00338697a84b.webp&sig=0VqxLM1oqU1A9YjQhv5u5THU4qI7pABcg6ElpT5E%2BnM%3D"
+        alt="Church"
+      />
       <h1>교회 소개</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie
-        suscipit nunc, ut iaculis nunc lobortis vitae. Sed malesuada eleifend
-        erat, vel congue dolor sollicitudin vel. Aliquam cursus mattis massa.
-        Nam elementum ex aliquet eros auctor, eu placerat nisi sagittis. Etiam
-        varius, nulla ac aliquet porttitor, augue mi eleifend felis, ut tempus
-        ipsum lectus in metus. Nam elementum semper sagittis. Morbi lacinia
-        condimentum diam sed blandit. Integer elementum lectus eu vestibulum
-        sollicitudin. Suspendisse potenti. Praesent ac nisi sit amet libero
-        interdum facilisis lacinia non neque. Mauris egestas venenatis eros, a
-        iaculis arcu mollis in. Donec odio purus, tristique at feugiat quis,
-        vulputate non orci. Praesent quis ante eu lectus lacinia volutpat
-        placerat at diam. Phasellus molestie pharetra ultrices. Phasellus
-        finibus tortor lacinia neque efficitur molestie. Duis lobortis sit amet
-        tellus vitae pellentesque.
+        "마르페"는 히브리어로 "건강, 치유, 회복"을 뜻합니다. 보스턴에 복음과
+        사랑으로 건강한 교회를 세우고 싶은 마음을 담아 성도들과 함께 교회 이름을
+        지었습니다.
+      </p>
+      <p>마르페 교회는?</p>
+      <ol>
+        <li>기독교 위기의 시대가운데 교회의 본질만을 추구하는 하나님의 교회</li>
+        <li>성도들이 함께 세워가는 하나님 나라의 공동체적 교회</li>
+        <li>하나님 나라의 가치와 생명을 세상에 흘려보내는 선교적 교회</li>
+        <li>
+          교회의 미래이자 소망인 청년과 아이들을 제자로 양육하는 비전의 교회
+        </li>
+        <li>하나님께서 세우신 가정을 회복하고 치유하는 회복의 교회</li>
+      </ol>
+      <p>
+        교회로서의 본질에 충실하고, 시대를 읽고, 새로운 세대를 주역으로 세우는
+        교회, 예배와 말씀, 그리고 선교에 생명을 거는 교회, 가정과 교회학교를
+        살리는 교회, 이름만 그리스도인이 아닌 진짜 그리스도의 제자를 만드는
+        교회, 우리가 꿈꾸는 교회입니다.
       </p>
     </>
   );
