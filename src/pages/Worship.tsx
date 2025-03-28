1c3bc1fa232b8fb5f14e4f39584c0a7b06b04308
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-27 23:46:51 -0400

    Add worship image

--- src/pages/Worship.tsx
+++ src/pages/Worship.tsx
@@ -1,13 +1,13 @@
 import "react";
 
-import church from "../assets/church.webp";
+import worshipImg from "../assets/worship.png";
 
 import "./Worship.css";
 
 export default function Worship() {
   return (
     <>
-      <img src={church} alt="Worship test" />
+      <img src={worshipImg} alt="Worship test" />
       <h1>찬양 플레이리스트</h1>
       <iframe
         src="https://www.youtube.com/embed/videoseries?si=jcbM7u0rPSMCORTw&amp;list=PL6VO8u2Zpfdfs9VLqEHkNvq0ICxdXN3j7"
