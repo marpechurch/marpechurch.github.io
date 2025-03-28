da99ec4a2793fe131da9ad9c398e630988d704d0
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-03-27 23:23:25 -0400

    Update routes

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -15,12 +15,12 @@ function AppHeader() {
   return (
     <Routes>
       <Route path="" element={<Home />} />
-      <Route path="church" element={<Church />} />
-      <Route path="community" element={<Community />} />
-      <Route path="service" element={<Service />} />
-      <Route path="videos" element={<Videos />} />
-      <Route path="worship" element={<Worship />} />
-      <Route path="offering" element={<Offering />} />
+      <Route path="/church" element={<Church />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/videos" element={<Videos />} />
+      <Route path="/worship" element={<Worship />} />
+      <Route path="/offering" element={<Offering />} />
     </Routes>
   );
 }
