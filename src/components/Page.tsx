78f979f1e1cf398c3faa3fb17016ced061dbdfb7
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-03 21:35:36 -0400

    Add Page component, refactor Pastor page

---
+++ src/components/Page.tsx
@@ -0,0 +1,38 @@
+import "react";
+import { Box, Grid2, Typography } from "@mui/material";
+
+interface PageProps {
+  content: string;
+  title: string;
+  image: string;
+}
+
+export default function Page({ content, image, title }: PageProps) {
+  return (
+    <Box>
+      <Typography variant="h1" sx={{ paddingBottom: 2 }}>
+        {title}
+      </Typography>
+      <Grid2
+        container
+        direction={{ md: "row", xs: "column-reverse" }}
+        spacing={2}
+      >
+        <Grid2 size={{ xs: 12, md: 8 }}>
+          <Typography>{content}</Typography>
+        </Grid2>
+        <Grid2 display="flex" justifyContent="center" size={{ xs: 12, md: 4 }}>
+          <Box
+            component="img"
+            src={image}
+            alt={`${title}`}
+            sx={{
+              height: "min-content",
+              maxWidth: 300,
+            }}
+          />
+        </Grid2>
+      </Grid2>
+    </Box>
+  );
+}
