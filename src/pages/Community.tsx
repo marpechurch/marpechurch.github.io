816bea548df5a9c182a06ecaa1a45c81e4908c1c
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-03 21:41:47 -0400

    Refactor Community page

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -1,30 +1,27 @@
 import "react";
+import { Typography } from "@mui/material";
 
 import communityImg from "../assets/community.webp";
-
-import "./Community.css";
+import Page from "../components/Page";
 
 export default function Community() {
-  return (
-    <>
-      <img src={communityImg} alt="Community" />
-      <h1>공동체</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie
-        suscipit nunc, ut iaculis nunc lobortis vitae. Sed malesuada eleifend
-        erat, vel congue dolor sollicitudin vel. Aliquam cursus mattis massa.
-        Nam elementum ex aliquet eros auctor, eu placerat nisi sagittis. Etiam
-        varius, nulla ac aliquet porttitor, augue mi eleifend felis, ut tempus
-        ipsum lectus in metus. Nam elementum semper sagittis. Morbi lacinia
-        condimentum diam sed blandit. Integer elementum lectus eu vestibulum
-        sollicitudin. Suspendisse potenti. Praesent ac nisi sit amet libero
-        interdum facilisis lacinia non neque. Mauris egestas venenatis eros, a
-        iaculis arcu mollis in. Donec odio purus, tristique at feugiat quis,
-        vulputate non orci. Praesent quis ante eu lectus lacinia volutpat
-        placerat at diam. Phasellus molestie pharetra ultrices. Phasellus
-        finibus tortor lacinia neque efficitur molestie. Duis lobortis sit amet
-        tellus vitae pellentesque.
-      </p>
-    </>
+  const content = (
+    <Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie
+      suscipit nunc, ut iaculis nunc lobortis vitae. Sed malesuada eleifend
+      erat, vel congue dolor sollicitudin vel. Aliquam cursus mattis massa. Nam
+      elementum ex aliquet eros auctor, eu placerat nisi sagittis. Etiam varius,
+      nulla ac aliquet porttitor, augue mi eleifend felis, ut tempus ipsum
+      lectus in metus. Nam elementum semper sagittis. Morbi lacinia condimentum
+      diam sed blandit. Integer elementum lectus eu vestibulum sollicitudin.
+      Suspendisse potenti. Praesent ac nisi sit amet libero interdum facilisis
+      lacinia non neque. Mauris egestas venenatis eros, a iaculis arcu mollis
+      in. Donec odio purus, tristique at feugiat quis, vulputate non orci.
+      Praesent quis ante eu lectus lacinia volutpat placerat at diam. Phasellus
+      molestie pharetra ultrices. Phasellus finibus tortor lacinia neque
+      efficitur molestie. Duis lobortis sit amet tellus vitae pellentesque.
+    </Typography>
   );
+
+  return <Page content={content} image={communityImg} title="공동체" />;
 }
