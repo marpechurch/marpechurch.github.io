029ce6863f335ece423aa82dd6dea24b7c42e05e
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-03 21:29:11 -0400

    Remove hooks

--- src/hooks/useIsMobile.ts
+++
@@ -1,7 +0,0 @@
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-
-export default function useIsMobile(): boolean {
-  const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down("sm"));
-}
