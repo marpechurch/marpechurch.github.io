4e6bfa32b6da930480461dd9965994ee3a39fb41
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-08 23:12:33 -0400

    Update phone number to email

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -7,7 +7,7 @@ export default function AppFooter() {
     <div className="AppFooter">
       <ul className="footerContent">
         <li>주소: 212 School St, Somerville, MA 02145</li>
-        <li>전화: 123-456-7890</li>
+        <li>이메일: marpechurch@gmail.com</li>
         <li>ⓒMarpe Church. All Rights Reserved.</li>
       </ul>
     </div>
