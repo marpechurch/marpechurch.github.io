6982c107a35d2e115639927993c74df1b4f382d1
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-12 14:00:05 -0400

    Update Church and Pastor text

--- src/pages/Church.tsx
+++ src/pages/Church.tsx
@@ -1,40 +1,41 @@
 import "react";
-import { List, ListItem, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import churchImg from "../assets/church.webp";
 import { Page } from "../components";
 
 export default function Church() {
   const content = (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
       <Typography>
-        "마르페"는 히브리어로 "건강, 치유, 회복"을 뜻합니다. 보스턴에 복음과
-        사랑으로 건강한 교회를 세우고 싶은 마음을 담아 성도들과 함께 교회 이름을
-        지었습니다.
+        마르페 교회는 2024년 10월 6일 바른 복음, 건강한 교회 라는 비전을 가진
+        성도들이 함께 모여 개척한 교회입니다. “마르페”라는 단어는 히브리어로
+        "건강, 치유, 회복"을 뜻합니다. 보스턴에 예수님의 복음과 사랑으로 건강한
+        교회를 세우고 싶은 마음을 담아 성도들과 함께 교회 이름을 지었습니다.
       </Typography>
-      <Typography variant="subtitle1">마르페 교회는?</Typography>
-      <List>
-        <ListItem>
-          기독교 위기의 시대가운데 교회의 본질만을 추구하는 하나님의 교회
-        </ListItem>
-        <ListItem>성도들이 함께 세워가는 하나님 나라의 공동체적 교회</ListItem>
-        <ListItem>
-          하나님 나라의 가치와 생명을 세상에 흘려보내는 선교적 교회
-        </ListItem>
-        <ListItem>
-          교회의 미래이자 소망인 청년과 아이들을 제자로 양육하는 비전의 교회
-        </ListItem>
-        <ListItem>
-          하나님께서 세우신 가정을 회복하고 치유하는 회복의 교회
-        </ListItem>
-      </List>
       <Typography>
-        교회로서의 본질에 충실하고, 시대를 읽고, 새로운 세대를 주역으로 세우는
-        교회, 예배와 말씀, 그리고 선교에 생명을 거는 교회, 가정과 교회학교를
-        살리는 교회, 이름만 그리스도인이 아닌 진짜 그리스도의 제자를 만드는
-        교회, 우리가 꿈꾸는 교회입니다.
+        마르페 교회는 기독교 위기의 시대가운데 교회의 본질을 추구하는 하나님이
+        주인이신 교회입니다.
       </Typography>
-    </>
+      <Typography>
+        성도들과 함께 세워가는 공동체적 교회이자 하나님 나라의 가치와 생명을
+        세상에 흘려보내는 선교적 교회입니다.
+      </Typography>
+      <Typography>
+        교회의 미래이자 소망인 청년들과 아이들을 제자로 양육하는 비전을 가진
+        비전의 교회입니다.
+      </Typography>
+      <Typography>
+        하나님께서 세우신 가정을 회복하고 치유하는 회복의 교회입니다.
+      </Typography>
+      <Typography>
+        이름만 그리스도인이 아닌 복음으로 세상을 변화시키는 진짜 그리스도의
+        제자를 세우고 훈련하는 제자 교회입니다.
+      </Typography>
+      <Typography>
+        이것이 우리 마르페 교회가 함께 꿈꾸고, 함께 만들어 가는 교회입니다.
+      </Typography>
+    </Box>
   );
 
   return <Page content={content} imageSrc={churchImg} title="교회 소개" />;
--- src/pages/Pastor.tsx
+++ src/pages/Pastor.tsx
@@ -1,19 +1,33 @@
 import "react";
-import { Typography } from "@mui/material";
+import { Box, List, ListItem, Typography } from "@mui/material";
 
 import pastorImg from "../assets/pastor.jpg";
 import { Page } from "../components";
 
 export default function Pastor() {
   const content = (
-    <Typography>
-      조정호 목사는 10년동안 보스턴에서 캠퍼스 선교사로 복음을 전했다. 고든콘웰
-      신학교에서 목회학 석사와 선교적 교회로 신학석사를 받았다. 미국 복음주의
-      교단인 ECA(Evangelical Church Alliance) 에서 안수를 받고 지역교회에서
-      청년들을 제자화 하는 사역을 했다. 하나님 말씀을 사랑하고 사람들을 만나
-      대화하는 것을 좋아한다. 보스턴에서 바른 복음으로 더불어 살아내는 건강한
-      공동체를 꿈꾸며, 마르페 교회를 성도들과 함께 개척했다.
-    </Typography>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+      <Typography>
+        조정호 목사는 2010년부터 10년동안 보스턴에서 캠퍼스 선교사로 복음을
+        전했습니다. 복음을 모르는 열방의 유학생들을 예수님께 인도하고 지역 교회
+        공동체로 연결하는 사역을 했습니다. 2016년, 사역을 하며 고든콘웰 신학교에
+        들어가 목회학석사와 선교적 교회로 신학석사를 받았습니다. 이후 지역교회
+        청년부를 담당하며 청년들과 젊은 부부들을 제자화 하는 사역을 했습니다.
+        미국 복음주의 교단인 C&MA(The Alliance) 교단 소속 목사로 바른 복음으로
+        더불어 살아내는 건강한 공동체를 꿈꾸며, 성도들과 함께 마르페 교회를
+        개척했습니다. 또한 특수교육을 전공한 김필영 사모와 혜성, 혜나, 유나 세
+        아이와 함께 믿음의 가정을 세워가고 있습니다.
+      </Typography>
+      <Typography variant="h6">약력</Typography>
+      <List>
+        <ListItem>
+          YWAM(Youth With A Mission) Boston 캠퍼스 사역 디렉터
+        </ListItem>
+        <ListItem>Northeastern University Spiritual Adviser</ListItem>
+        <ListItem>보스턴 온누리 교회 부목사</ListItem>
+        <ListItem>현) 마르페 교회 담임목사</ListItem>
+      </List>
+    </Box>
   );
 
   return <Page content={content} imageSrc={pastorImg} title="목사님 소개" />;
