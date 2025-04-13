a1d6fc117dcd996c63f1d1984b3c09ad8bfccb37
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-13 13:30:59 -0400

    Update denomination

--- src/pages/Denomination.tsx
+++ src/pages/Denomination.tsx
@@ -1,27 +1,35 @@
 import "react";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-import denominationImg from "../assets/denomination.png";
-import { Page } from "../components";
+import { Page, YouTube } from "../components";
+import {
+  DENOMINATION_ENGLISH_URL,
+  DENOMINATION_KOREAN_URL,
+} from "../utils/constants";
 
 export default function Denomination() {
   const content = (
-    <Typography>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie
-      suscipit nunc, ut iaculis nunc lobortis vitae. Sed malesuada eleifend
-      erat, vel congue dolor sollicitudin vel. Aliquam cursus mattis massa. Nam
-      elementum ex aliquet eros auctor, eu placerat nisi sagittis. Etiam varius,
-      nulla ac aliquet porttitor, augue mi eleifend felis, ut tempus ipsum
-      lectus in metus. Nam elementum semper sagittis. Morbi lacinia condimentum
-      diam sed blandit. Integer elementum lectus eu vestibulum sollicitudin.
-      Suspendisse potenti. Praesent ac nisi sit amet libero interdum facilisis
-      lacinia non neque. Mauris egestas venenatis eros, a iaculis arcu mollis
-      in. Donec odio purus, tristique at feugiat quis, vulputate non orci.
-      Praesent quis ante eu lectus lacinia volutpat placerat at diam. Phasellus
-      molestie pharetra ultrices. Phasellus finibus tortor lacinia neque
-      efficitur molestie. Duis lobortis sit amet tellus vitae pellentesque.
-    </Typography>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+      <Typography>
+        C&MA는 The Christian And Missionary Alliance의 약자이며 세계의 복음화를
+        위해 전 세계 90개 국가의 2만 5천여 교회, 1천200명의 선교사와 7백만의
+        신실한 신자들이 함께 동역하고 있습니다. The Christian And Missionary
+        Alliance (C&MA)는 1887년에 심슨(A. B. Simpson) 목사님을 중심으로
+        뉴욕시의 맨하탄에서 7명이 예배 드리는 것으로 시작되었습니다. 거기서부터
+        복음이 아프리카로, 중국으로, 그리고 전 세계로 전파되기 시작했습니다.
+        C&MA 교단은 다른 교단들이 한국에 선교사를 보낼 때 이미 많은 교단들이
+        들어가서 선교사역을 하고 있는 한국에 선교사를 파송하지 않았으며, 대신
+        일본에는 신학교 및 선교사를 파송했었고 일본에 ‘동양선교회’라는 신학교를
+        세웠습니다. 그곳에서 신학을 공부했던 한국인 목사님이 한국으로 돌아가
+        세운 교단이 성결교단입니다. 한국의 성결 교단의 모체가 바로 C&MA
+        교단입니다. 한국에 많이 알려진 A.W. 토저 목사님과 빌리 그래함 목사님도
+        C&MA 교단 출신 목사님이셨습니다. C&MA 교단은 현재도 전세계 복음화를 위해
+        최선을 다하고 있는 대표적 복음주의 교단입니다.
+      </Typography>
+      <YouTube src={DENOMINATION_KOREAN_URL} title="C&MA 교단 소개 (한국어)" />
+      <YouTube src={DENOMINATION_ENGLISH_URL} title="C&MA 교단 소개 (영어)" />
+    </Box>
   );
 
-  return <Page content={content} imageSrc={denominationImg} title="교단 소개" />;
+  return <Page content={content} title="교단 소개" />;
 }
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,4 +1,8 @@
 // LINKS
+export const DENOMINATION_ENGLISH_URL =
+  "https://www.youtube.com/embed/YLOwBeoVTb4?si=D_Iv3TznMbBzXQx9";
+export const DENOMINATION_KOREAN_URL =
+  "https://www.youtube.com/embed/6m-RlfrWghs?si=xKf3dw-dKXuRj7iQ";
 export const DIRECTIONS_URL =
   "https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d2946.8132762991086!2d-71.09959287382455!3d42.38912408326484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x89e370d2ae34fbe1%3A0x50490ba6a05384d2!2sGilman%20Square%2C%20Somerville%2C%20MA%2002143!3m2!1d42.38793!2d-71.09676999999999!4m5!1s0x89e370d339b6db33%3A0xedddf27973fb9585!2sIgreja%20Presbiteriana%20de%20Boston%2C%20School%20Street%2C%20Somerville%2C%20MA!3m2!1d42.390674!2d-71.09634729999999!5e0!3m2!1sen!2sus!4v1743128778864!5m2!1sen!2sus";
 export const INSTAGRAM_URL = "https://www.instagram.com/marpechurch/";
@@ -10,4 +14,5 @@ export const SERMONS_URL =
   "https://www.youtube.com/embed/videoseries?list=PL6VO8u2Zpfdf_UzfAjmyCaPLCiDK-XviS";
 export const WORSHIP_URL =
   "https://www.youtube.com/embed/videoseries?si=jcbM7u0rPSMCORTw&amp;list=PL6VO8u2Zpfdfs9VLqEHkNvq0ICxdXN3j7";
-export const ZELLE_URL = "https://enroll.zellepay.com/qr-codes?data=eyJuYW1lIjoiTUFSUEUgQ0hVUkNIIiwidG9rZW4iOiJtYXJwZWNodXJjaEBnbWFpbC5jb20ifQ=="
+export const ZELLE_URL =
+  "https://enroll.zellepay.com/qr-codes?data=eyJuYW1lIjoiTUFSUEUgQ0hVUkNIIiwidG9rZW4iOiJtYXJwZWNodXJjaEBnbWFpbC5jb20ifQ==";
