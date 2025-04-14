ccb15d96f3e447fe57e5e9fbe56647f13c1901e6
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-13 20:33:49 -0400

    Add pastoral visit to Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import "react";
-import { Typography, Link } from "@mui/material";
+import { Link, List, ListItem } from "@mui/material";
 
 import registerImg from "../assets/register.png";
 import { Page } from "../components";
@@ -7,13 +7,19 @@ import { REGISTER_URL } from "../utils/constants";
 
 export default function Register() {
   const content = (
-    <Typography>
-      본 교회에 등록을 원하시는 분들은 이{" "}
-      <Link href={REGISTER_URL} target="_blank" rel="noopener noreferrer">
-        QR코드로
-      </Link>{" "}
-      작성해 주시기 바랍니다.
-    </Typography>
+    <List>
+      <ListItem>
+        본 교회에 등록을 원하시는 분들은 이&nbsp;
+        <Link href={REGISTER_URL} target="_blank" rel="noopener noreferrer">
+          QR 코드
+        </Link>{" "}
+        로 작성해 주시기 바랍니다.
+      </ListItem>
+      <ListItem>
+        심방 신청을 원하시는 분들은 교회 이메일(marpechurch@gmail.com)로
+        연락주시기 바랍니다.
+      </ListItem>
+    </List>
   );
 
   return (
