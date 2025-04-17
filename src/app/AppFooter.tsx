cc5a00dbd5dc5d52e027e889904c7a878ce5b79e
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-16 22:43:29 -0400

    Update AppFooter to MUI

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -1,21 +1,27 @@
 import "react";
-
-import "./AppFooter.css";
-import { Link } from "@mui/material";
+import { Box, Link, List, ListItem } from "@mui/material";
 
 export default function AppFooter() {
   return (
-    <div className="AppFooter">
-      <ul className="footerContent">
-        <li>
+    <Box
+      sx={{
+        borderTop: "1px solid #ccc",
+        fontSize: 12,
+        padding: 1,
+      }}
+    >
+      <List sx={{ padding: 0 }}>
+        <ListItem sx={{ padding: 0 }}>
           주소:{" "}
           <Link href="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5893.482904781368!2d-71.096441!3d42.390654!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e370d339b6db33%3A0xedddf27973fb9585!2sIgreja%20Presbiteriana%20de%20Boston!5e0!3m2!1sen!2sus!4v1744601459887!5m2!1sen!2sus">
             212 School St, Somerville, MA 02145
           </Link>
-        </li>
-        <li>이메일: marpechurch@gmail.com</li>
-        <li>ⓒMarpe Church. All Rights Reserved.</li>
-      </ul>
-    </div>
+        </ListItem>
+        <ListItem sx={{ padding: 0 }}>이메일: marpechurch@gmail.com</ListItem>
+        <ListItem sx={{ padding: 0 }}>
+          ⓒMarpe Church. All Rights Reserved.
+        </ListItem>
+      </List>
+    </Box>
   );
 }
