2a7de75e377e209cffd782e01170a1afc7bd9ff9
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-16 22:50:55 -0400

    Update email to link in Register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -13,13 +13,18 @@ export default function Register() {
           본 교회에 등록을 원하시는 분들은 이&nbsp;
           <Link href={REGISTER_FORM} target="_blank" rel="noopener noreferrer">
             QR 코드
-          </Link>{" "}
+          </Link>
           로 작성해 주시기 바랍니다.
         </Typography>
       </ListItem>
       <ListItem>
-        심방 신청을 원하시는 분들은 교회 이메일(marpechurch@gmail.com)로
-        연락주시기 바랍니다.
+        <Typography>
+          심방 신청을 원하시는 분들은 교회 이메일(
+          <Link href="mailto:marpechurch@gmail.com" underline="hover">
+            marpechurch@gmail.com
+          </Link>
+          )로 연락주시기 바랍니다.
+        </Typography>
       </ListItem>
     </List>
   );
