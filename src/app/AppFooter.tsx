be671215f0778fe3d367093ef2651104f643b7f9
Yehoon Joo <yehoon.joo@bitsighttech.com>
2025-04-16 22:49:25 -0400

    Update footer email to link

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -12,12 +12,17 @@ export default function AppFooter() {
     >
       <List sx={{ padding: 0 }}>
         <ListItem sx={{ padding: 0 }}>
-          주소:{" "}
+          주소:&nbsp;
           <Link href="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5893.482904781368!2d-71.096441!3d42.390654!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e370d339b6db33%3A0xedddf27973fb9585!2sIgreja%20Presbiteriana%20de%20Boston!5e0!3m2!1sen!2sus!4v1744601459887!5m2!1sen!2sus">
             212 School St, Somerville, MA 02145
           </Link>
         </ListItem>
-        <ListItem sx={{ padding: 0 }}>이메일: marpechurch@gmail.com</ListItem>
+        <ListItem sx={{ padding: 0 }}>
+          이메일:&nbsp;
+          <Link href="mailto:marpechurch@gmail.com" underline="hover">
+            marpechurch@gmail.com
+          </Link>
+        </ListItem>
         <ListItem sx={{ padding: 0 }}>
           ⓒMarpe Church. All Rights Reserved.
         </ListItem>
