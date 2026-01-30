7296134583a9de13aac5d0c7f07275357e809992
Yehoon Joo <yehoon.joo@bitsighttech.com>
2026-01-30 13:19:35 -0500

    Remove church image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,11 @@ import "react";
 import { Box, Typography, Paper } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
-import churchImg from "../assets/church.webp";
 import { Page } from "../components";
-import { SQUARE_IMAGE_STYLE } from "../utils/constants";
 
 export default function Home() {
   const { t } = useTranslation();
-  
+
   const content = (
     <Paper elevation={1} sx={{ p: { xs: 1.5, md: 3 } }}>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
@@ -22,57 +20,17 @@ export default function Home() {
               mb: 2,
             }}
           >
-{t("home.title")}
+            {t("home.title")}
           </Typography>
 
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-              gap: 3,
-              alignItems: { xs: "center", md: "flex-start" },
-            }}
-          >
-            {/* 교회 사진 */}
-            <Box sx={SQUARE_IMAGE_STYLE}>
-              <Box
-                component="img"
-                src={churchImg}
-                alt="보스턴 마르페 교회"
-                sx={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            </Box>
-
-            {/* 교회 소개글 */}
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: 1, flex: 1 }}
-            >
-              <Typography>
-                {t("home.intro.paragraph1")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph2")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph3")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph4")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph5")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph6")}
-              </Typography>
-              <Typography>
-                {t("home.intro.paragraph7")}
-              </Typography>
-            </Box>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography>{t("home.intro.paragraph1")}</Typography>
+            <Typography>{t("home.intro.paragraph2")}</Typography>
+            <Typography>{t("home.intro.paragraph3")}</Typography>
+            <Typography>{t("home.intro.paragraph4")}</Typography>
+            <Typography>{t("home.intro.paragraph5")}</Typography>
+            <Typography>{t("home.intro.paragraph6")}</Typography>
+            <Typography>{t("home.intro.paragraph7")}</Typography>
           </Box>
         </Box>
 
@@ -81,9 +39,7 @@ export default function Home() {
           <Typography variant="h5" sx={{ mb: 2, fontWeight: "bold" }}>
             {t("home.marpeMeaning.title")}
           </Typography>
-          <Typography>
-            {t("home.marpeMeaning.description")}
-          </Typography>
+          <Typography>{t("home.marpeMeaning.description")}</Typography>
         </Box>
       </Box>
     </Paper>
